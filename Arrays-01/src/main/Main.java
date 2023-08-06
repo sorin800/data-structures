cb1d2efcdfb11cd7a168b6bc56d3ab8223cc2d08
sorin.truica <sorin.truica@loblaw.ca>
2023-08-06 20:35:01 +0300

    added arrays folder

---
+++ Arrays-01/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SingleDimensionArray sda = new SingleDimensionArray(10);
+        sda.insert(0, 10);
+        sda.insert(1, 20);
+        sda.insert(2, 30);
+//        sda.insert(1,30);
+//        sda.insert(12,120);
+
+        var firstElement = sda.arr[0];
+        System.out.println(firstElement);
+        var thirdElement = sda.arr[3];
+        System.out.println(thirdElement);
+
+    }
+}
---
+++ Arrays-01/src/main/SingleDimensionArray.java
@@ -0,0 +1,35 @@
+package main;
+
+public class SingleDimensionArray {
+
+    int arr[] = null;
+
+    public SingleDimensionArray(int sizeOfArray) {
+        arr = new int[sizeOfArray];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.MIN_VALUE;
+        }
+    }
+
+    public void insert(int location, int valueToBeInserted) {
+        try {
+            if (arr[location] == Integer.MIN_VALUE) {
+                arr[location] = valueToBeInserted;
+                System.out.println("Successfully inserted");
+            } else {
+                System.out.println("This cell is already occupied");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index to access array!");
+        }
+
+    }
+
+    // Array Traversal
+    public void traverseArray() {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
